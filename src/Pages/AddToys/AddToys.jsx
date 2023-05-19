e02b5cd2c62d6c5f11ad7a1953a2f2edb571e0b2
Ziaul-hasan <ziaulhasanshaon006@gmail.com>
2023-05-19 19:00:48 +0600

    toy add form done

--- src/Pages/AddToys/AddToys.jsx
+++ src/Pages/AddToys/AddToys.jsx
@@ -1,10 +1,86 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import './AddToy.css'
 
 const AddToys = () => {
     return (
         <div>
-            <h2>you will add toys here</h2>
+            <div className='w-4/5 md:w-3/5 mx-auto my-12 p-8 shadow-xl rounded-lg bg-form'>
+                <h1 className='my-5 text-xl md:text-4xl font-paytonOne font-semibold text-center text-slate-600'>Add Your Toy Here</h1>
+                <form >
+                    <div className='grid md:grid-cols-2 gap-5 my-4'>
+                        <div className="form-control">
+                            <label className="label font-secularOne">
+                                <span className="label-text">Picture</span>
+                            </label>
+                            <input type="text" name='picture' placeholder="picture url of toy" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label font-secularOne">
+                                <span className="label-text">Toy Name</span>
+                            </label>
+                            <input type="text" name='toy' placeholder="name of toy" className="input input-bordered" />
+                        </div>
+                    </div>
+                    <div className='grid md:grid-cols-2 gap-5 my-4'>
+                        <div className="form-control">
+                            <label className="label font-secularOne">
+                                <span className="label-text">Seller Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="seller name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label font-secularOne">
+                                <span className="label-text">Seller Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="seller email" className="input input-bordered" />
+                        </div>
+                    </div>
+                    <div className='grid md:grid-cols-2 gap-5 my-4'>
+                        <div className="form-control">
+                            <label className="label font-secularOne">
+                                <span className="label-text">Available Quantity</span>
+                            </label>
+                            <input type="text" name='quantity' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label font-secularOne">
+                                <span className="label-text">Sub-Category</span>
+                            </label>
+                            <select name="subcategory" className='input input-bordered'>
+                                <option value="">Select One</option>
+                                <option value="car">Car</option>
+                                <option value="suv">SUV</option>
+                                <option value="bus">Bus</option>
+                                <option value="truck">Truck</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className='grid md:grid-cols-2 gap-5 my-4'>
+                        <div className="form-control">
+                            <label className="label font-secularOne">
+                                <span className="label-text">Price of Toy</span>
+                            </label>
+                            <input type="text" name='price' placeholder="write price" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label font-secularOne">
+                                <span className="label-text">Ratings</span>
+                            </label>
+                            <input type="text" name='ratings' placeholder="ratings" className="input input-bordered" />
+                        </div>
+                    </div>
+                    <div className="form-control">
+                        <label className="label font-secularOne">
+                            <span className="label-text">Description</span>
+                        </label>
+                        <textarea name="messege" id="" cols="30" rows="4" placeholder='Describe about toy' className='border border-slate-300 p-4 rounded-md'></textarea>
+                    </div>
+                    <div className="form-control mt-6">
+                        <input type="submit" value="Add Toy" className="btn bg-lime-600 border-0 font-secularOne" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,40 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleLogin = event => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+    }
     return (
         <div>
-            <h2>Login form will come soon</h2>
+            <div className='w-4/5 md:w-2/5 mx-auto my-12 p-8 shadow-xl rounded-lg bg-form'>
+                <h1 className='my-5 text-xl md:text-4xl font-paytonOne font-semibold text-center text-slate-600'>Please Login !!</h1>
+                <form onSubmit={handleLogin}>
+                    <div className="form-control">
+                        <label className="label font-secularOne">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label font-secularOne">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                    </div>
+                    <div className="form-control my-6">
+                        <input type="submit" value="Login" className="btn border-0 bg-lime-600" />
+                    </div>
+                    <p className='text-base md:text-lg font-secularOne font-medium text-center'>New to Toyland Cars? <Link to='/register' className='text-lime-600'>Register</Link> First</p>
+                </form>
+                <div className="divider my-5">OR</div>
+            </div>
         </div>
     );
 };
