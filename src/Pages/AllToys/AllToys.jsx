8ffa0d7827bc291adee9fc26a9433622ac4a0ab6
Ziaul-hasan <ziaulhasanshaon006@gmail.com>
2023-05-20 12:00:22 +0600

    all toys page done

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,11 +1,43 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Toytable from './Toytable/Toytable';
 
 const AllToys = () => {
+    const [toys, setToys] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/toys')
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }, [])
     return (
-        <div>
-            <h2>all toys will show here</h2>
-        </div>
+        <>
+            <div>
+                <h2>all toys will show here</h2>
+            </div>
+            <div className='mx-5 md:mx-20 my-10 shadow-lg rounded-lg'>
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Picture</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Name</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Sub-category</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>seller name</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Available Qty</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Price</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Show more</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            toys?.map(toy => <Toytable key={toy._id} toy={toy}></Toytable>)
+                        }
+                    </tbody>
+                </table>
+
+            </div>
+
+        </>
     );
 };
 
---
+++ src/Pages/AllToys/Toytable/Toytable.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Toytable = ({ toy }) => {
+    const { _id, picture, name, sellerName, price, quantity, subcategory } = toy || {}
+    return (
+        <tr>
+            <td>
+                <div className='p-5'>
+                    <img className='w-20 h-12 md:w-40 md:h-32 rounded-3xl' src={picture} alt="Avatar Tailwind CSS Component" />
+                </div>
+            </td>
+            <td className='text-slate-600 font-secularOne capitalize text-lg'>
+                <p>{name}</p>
+            </td>
+            <td className='text-slate-600 font-secularOne capitalize text-lg text-center'>{subcategory}</td>
+            <td className='text-slate-600 font-secularOne capitalize text-lg'>{sellerName}</td>
+            <td className='text-slate-600 font-secularOne capitalize text-lg text-center'>{quantity}</td>
+            <td className='text-slate-600 font-secularOne capitalize text-lg text-center'>$ {price}</td>
+            <th className='text-end'>
+                <button className="btn bg-lime-600 border-0 font-secularOne font-medium">details</button>
+            </th>
+        </tr>
+
+    );
+};
+
+export default Toytable;
