1c198e44d67f09f54b804305e672afb8ec44d1ad
Ziaul-hasan <ziaulhasanshaon006@gmail.com>
2023-05-20 14:50:58 +0600

    mytoy page done

--- src/Pages/MyToys/MyTos.jsx
+++ src/Pages/MyToys/MyTos.jsx
@@ -1,11 +1,48 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Providers/AuthProviders';
+import MytoyTable from './MytoyTable/MytoyTable';
 
 const MyTos = () => {
+    const {user} = useContext(AuthContext)
+    const [myToys, setMytoys] = useState([])
+
+    const url = `http://localhost:5000/toys?email=${user.email}`
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setMytoys(data))
+    }, [url])
     return (
-        <div>
-            
-        </div>
+        <>
+            <div>
+                <h2>all toys will show here</h2>
+            </div>
+            <div className='mx-5 md:mx-20 my-10 shadow-lg rounded-lg'>
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Delete</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Picture</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Name</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Sub-category</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>seller name</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Available Qty</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Price</th>
+                            <th className='text-base md:text-lg font-paytonOne capitalize font-medium text-slate-600'>Show more</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myToys?.map(myToy => <MytoyTable key={myToy._id} myToy={myToy}></MytoyTable>)
+                        }
+                    </tbody>
+                </table>
+
+            </div>
+
+        </>
     );
 };
 
--- src/Pages/MyToys/MytoyTable/MytoyTable.jsx
+++ src/Pages/MyToys/MytoyTable/MytoyTable.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
-const Toytable = ({ toy }) => {
-    const { _id, picture, name, sellerName, price, quantity, subcategory } = toy || {}
+const MytoyTable = ({ myToy }) => {
+    const { _id, picture, name, sellerName, price, quantity, subcategory } = myToy || {}
     return (
         <tr>
+            <td>
+                <button className="p-2 border-2 border-lime-600 rounded-full">
+                    <FaTrashAlt className='text-red-600 text-2xl'></FaTrashAlt>
+                </button>
+            </td>
             <td>
                 <div className='p-5'>
                     <img className='w-20 h-12 md:w-40 md:h-32 rounded-3xl' src={picture} alt="Avatar Tailwind CSS Component" />
@@ -17,12 +23,12 @@ const Toytable = ({ toy }) => {
             <td className='text-slate-600 font-secularOne capitalize text-sm md:text-lg'>{sellerName}</td>
             <td className='text-slate-600 font-secularOne capitalize text-sm md:text-lg text-center'>{quantity}</td>
             <td className='text-slate-600 font-secularOne capitalize text-sm md:text-lg text-center'>$ {price}</td>
-            <th className='text-end'>
-                <button className="btn bg-lime-600 border-0 font-secularOne font-medium">details</button>
+            <th className='text-end space-y-3'>
+                <button className="btn bg-lime-600 border-0 font-secularOne font-medium block">details</button>
+                <button className="btn bg-lime-600 border-0 font-secularOne font-medium block">update</button>
             </th>
         </tr>
-
     );
 };
 
-export default Toytable;
+export default MytoyTable;
